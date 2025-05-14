8719fea07e894da8d1748bef69eca946fa605aff
Paulo Carbajal <paulo.carbajal@cuida.pe>
2025-05-14 18:39:53 -0500

    Add: agregando el puerto parametrizable app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './infrastructure/http/server';
 
-const PORT = 3000;
+const PORT = Number(process.env.PORT_APP) || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
